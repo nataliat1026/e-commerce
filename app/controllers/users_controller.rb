dc9e580c52ae436a4a3983ed41f9e583f2938a40
Natalia Teixeira <nataliat1026@gmail.com>
2022-07-03 16:30:01 -0400

    user CRUD functionality done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+    #   GET /users/:id
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    #   POST /users
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    #   PATCH /users/:id
+    def update
+        user = User.find(params[:id])
+        user.update!(user_params)
+        render json: user, status: :created
+    end
+
+    #   DELETE /users/:id
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        head :no_content
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:email, :password)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :images, only: []
   resources :reviews, only: [:create, :update, :destroy]
-  resources :users, only: []
+  resources :users, only: [:show, :create, :update, :destroy]
   resources :category_tags, only: []
   resources :categories, only: [:show]
   resources :products, only: [:index, :show]
