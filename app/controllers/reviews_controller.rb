0e7672ba8e2b4937ae1fb4eabe5413847f330019
Natalia Teixeira <nataliat1026@gmail.com>
2022-07-03 15:08:24 -0400

    review create, update, destroy functinoality done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,31 @@
 class ReviewsController < ApplicationController
 
-    
+    #   POST /reviews
+    def create
+        review = Review.create!(review_params)
+        render json: review, status: :created
+    end
+
+    #   PATCH /reviews/:id
+    def update
+        review = Review.find(params[:id])
+        review.update!(review_params)
+        render json: review, status: :created
+    end
+
+    #   DELETE /reviews/:id
+    def destroy
+        review = Review.find(params[:id])
+        review.destroy
+        head :no_content
+    end
+
+    private
+
+    def review_params
+        params.permit(:rating, :content)
+    end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :images, only: []
-  resources :reviews, only: []
+  resources :reviews, only: [:create, :update, :destroy]
   resources :users, only: []
   resources :category_tags, only: []
   resources :categories, only: [:show]
