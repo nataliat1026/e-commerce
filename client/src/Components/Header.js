e87c7ea8204fa4727b9fc0121fdd77b89a4bbf92
Natalia Teixeira <nataliat1026@gmail.com>
2022-07-12 18:12:36 -0400

    search bar refined, fixed styling issue, added right-side nav elements

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -6,12 +6,36 @@ const Header = () => {
   return (
     <header>
       <div className='headerTopWrapper'>
-        <Link to='/'>
+        <Link className='logoLink' to='/'>
           <img className='logo' src='https://i.ibb.co/W04cRMH/logo-light.png' alt="amazon logo with word 'not' in front of it"></img>
         </Link>
         <div className='searchWrapper'>
           <input className='searchBar' type='text'></input>
           <SearchIcon className='searchIcon' />
+        </div>
+        <div className='headerTextWrapper'>
+          <div className='headerTextDiv'>
+            <p className='headerText'>
+              Greeting
+            </p>
+            <p className='headerText'>
+              Account &amp; Lists
+            </p>
+          </div>
+          <div className='headerTextDiv'>
+            <p className='headerText'>
+              Returns
+            </p>
+            <p className='headerText'>
+              &amp; Orders
+            </p>
+          </div>
+          <div className='headerTextDiv'>
+            <ShoppingCartIcon className='shoppingCartIcon' />
+            <p className='headerText'>
+              Basket
+            </p>
+          </div>
         </div>
       </div>
 
