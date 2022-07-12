ec251118f1f76792785e56fceccf0e49c84af470
Natalia Teixeira <nataliat1026@gmail.com>
2022-07-12 16:57:11 -0400

    header progress linked logo and added search bar and search icon

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
 
 const Header = () => {
   return (
     <header>
-      <div>
-        <div>
-          <img className='fullLogo' src='https://i.ibb.co/W04cRMH/logo-light.png' alt="amazon logo with word 'not' in front of it"></img>
+      <div className='headerTopWrapper'>
+        <Link to='/'>
+          <img className='logo' src='https://i.ibb.co/W04cRMH/logo-light.png' alt="amazon logo with word 'not' in front of it"></img>
+        </Link>
+        <div className='searchWrapper'>
+          <input className='searchBar' type='text'></input>
+          <SearchIcon className='searchIcon' />
         </div>
       </div>
+
+
 
       <div>
 
@@ -17,4 +25,3 @@ const Header = () => {
 }
 
 export default Header
-
