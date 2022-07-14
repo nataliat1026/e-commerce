5d9f4b038a88430823d2d6c77caa0e2e124a569e
Natalia Teixeira <nataliat1026@gmail.com>
2022-07-14 16:09:13 -0400

    styled card details and add to cart button

--- client/src/Components/ProductCard.js
+++ client/src/Components/ProductCard.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StarIcon } from '@heroicons/react/solid';
 
 const ProductCard = ({ id, name, price, description, images, rating }) => {
+    const [prime] = useState(Math.random() < 0.5)
 
-    console.log(rating)
 
     return (
-        
-
-        <div>
-            <img className='productThumbnail' src={images[0].url} alt='gallery of product images'></img>
-            <h4>{name}</h4>
-
+        <div className='productCard'>
+            <img className='cardImg' src={images[0].url} alt='gallery of product images'></img>
+            <p className='cardName'>{name}</p>
             <div className='ratingContainer'>
                 {Array(rating)
                     .fill()
@@ -19,8 +16,19 @@ const ProductCard = ({ id, name, price, description, images, rating }) => {
                         <StarIcon className='starIcon' />
                     ))}
             </div>
+            <p className='cardDescription'>{description}</p>
+            <p className='cardPrice'>${price}</p>
+            
+            {prime && (
+                <div className='prime'>
+                    <img className='primeImg' src='https://whitebox.com/wp-content/uploads/2020/05/Prime-tag-.png' alt='Amazon Prime logo'></img>
+                    <p className='primeTxt'>FREE Next-day Delivery</p>
+                </div>
+            )}
 
-                
+            <button className='cardBtn'>Add to Cart</button>
+
+
         </div>
     )
     }
